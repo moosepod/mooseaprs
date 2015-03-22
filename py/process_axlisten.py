4dd75685a562624b867034ac3ba663205adbcfb1
Matthew Christensen <mchristensen@moosepod.com>
2015-03-22 22:53:09 +0000

    Basic script and sample data

---
+++ py/process_axlisten.py
@@ -0,0 +1,8 @@
+""" Takes the data on STDIN and processes it, looking for packets we are interested in """
+import sys
+
+print '>> STARTING'
+while 1:
+   line = sys.stdin.readline()
+   print line
+print '>> DONE'
